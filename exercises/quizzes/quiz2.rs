820ed3d79ab44c3559a8f197a4abb6163d1c93c2
Luis Marques (CTW) <Luis.Marques@ctw.bmwgroup.com>
2024-10-26 14:42:32 +0100

    completed till generics

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -26,8 +26,19 @@ enum Command {
 mod my_module {
     use super::Command;
 
-    // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        input.into_iter()
+            .map(|(string, command)| { transform(string, command) })
+            .collect()
+    }
+
+    fn transform(string: String, command: Command) -> String {
+        match command {
+            Command::Uppercase => string.to_uppercase(),
+            Command::Trim => string.trim().to_string(),
+            Command::Append(times) => string + "bar".repeat(times).as_ref()
+        }
+    }
 }
 
 fn main() {
@@ -36,8 +47,7 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
